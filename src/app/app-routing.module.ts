39816169f2ca78caacdd2548c1e8eea08a832366
PheletsoBear <mashalaneevanspheletso@gmail.com>
2023-09-12 17:31:17 +0200

    created the skeleton of E-commerce

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/Home/home.component';
+import { AccountComponent } from './components/Account/account.component';
+import { CartComponent } from './components/Cart/cart.component';
+import { LoginComponent } from './components/Login/login.component';
+import { LogOutComponent } from './components/LogOut/log-out.component';
+import { OrdersComponent } from './components/Orders/orders.component';
+import { ProductsComponent } from './components/Products/products.component';
+import { SignUpComponent } from './components/SignUp/sign-up.component';
+import { NotFoundComponent } from './components/Not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+     { path: '',         component: HomeComponent},
+     { path: 'Account',  component: AccountComponent},
+     {path: 'Cart',      component: CartComponent},
+     {path:'Login',      component: LoginComponent},
+     {path:'LogOut' ,    component: LogOutComponent},
+     {path: 'SignUp',    component:SignUpComponent},
+     {path:'Orders',     component: OrdersComponent},
+     {path:'Products',   component:ProductsComponent},
+     {path: '**' ,       component: NotFoundComponent}
+
+     
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,36 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HomeComponent } from './components/Home/home.component';
+import { SignUpComponent } from './components/SignUp/sign-up.component';
+import { LoginComponent } from './components/Login/login.component';
+import { LogOutComponent } from './components/LogOut/log-out.component';
+import { CartComponent } from './components/Cart/cart.component';
+import { AccountComponent } from './components/Account/account.component';
+import { OrdersComponent } from './components/Orders/orders.component';
+import { ProductsComponent } from './components/Products/products.component';
+import { NavbarComponent } from './components/Navbar/navbar.component';
+import { NotFoundComponent } from './components/Not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SignUpComponent,
+    LoginComponent,
+    LogOutComponent,
+    CartComponent,
+    AccountComponent,
+    OrdersComponent,
+    ProductsComponent,
+    NavbarComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
